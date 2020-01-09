47f0f079861527e4f77c8e711f8909a62d2ac389
Yentl <yenty@tari.com>
2020-01-09 13:04:50 +0200

    Using timezone from venue for future date

--- src/components/pages/admin/events/EventUpdate.js
+++ src/components/pages/admin/events/EventUpdate.js
@@ -345,7 +345,7 @@ class Event extends Component {
 		const { classes } = this.props;
 		const { event } = eventUpdateStore;
 		const hasPublishDate = !!event.publishDate;
-
+		const timezone = event.eventDate._z ? event.eventDate._z : "";
 		return (
 			<div>
 				<FormSubHeading>Publish options</FormSubHeading>
@@ -365,7 +365,7 @@ class Event extends Component {
 						active={hasPublishDate}
 						onClick={() =>
 							eventUpdateStore.updateEvent({
-								publishDate: moment()
+								publishDate: moment().tz(timezone.name)
 							})
 						}
 					>
@@ -443,7 +443,7 @@ class Event extends Component {
 							})
 						}
 					>
-						Published
+						{moment.utc(event.publishDate).isBefore(moment.utc()) ? "Published" : "Draft"}
 					</RadioButton>
 					<RadioButton
 						active={shouldUnpublish}
@@ -459,8 +459,8 @@ class Event extends Component {
 
 				{!shouldUnpublish ? (
 					<Typography className={classes.publishedAt}>
-						Published at{" "}
-						{moment(event.publishDate).format("MM/DD/YYYY hh:mm A z")}
+						{moment.utc(event.publishDate).isBefore(moment.utc()) ? "Published at " : "Scheduled "}
+						{moment(event.publishDate).format("MM/DD/YYYY hh:mm A (z)")}
 					</Typography>
 				) : null}
 			</div>
